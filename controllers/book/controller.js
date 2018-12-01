ac77b3f3460df31b8c278b23000b5df624e6b715
Vladimir Suvorov <bluurn@gmail.com>
2018-12-01 21:50:07 +0300

    rename book action names

--- controllers/book/controller.js
+++ controllers/book/controller.js
@@ -1,19 +1,19 @@
-import book_list from './actions/index';
-import book_detail from './actions/show';
-import book_create_get from './actions/create_form';
-import book_create_post from './actions/create';
-import book_delete_get from './actions/delete_form';
-import book_delete_post from './actions/delete';
-import book_update_get from './actions/update_form';
-import book_update_post from './actions/update';
+import index from './actions/index';
+import show from './actions/show';
+import create_form from './actions/create_form';
+import create from './actions/create';
+import destroy_form from './actions/destroy_form';
+import destroy from './actions/destroy';
+import update_form from './actions/update_form';
+import update from './actions/update';
 
 export default {
-  book_list,
-  book_detail,
-  book_create_get,
-  book_create_post,
-  book_delete_get,
-  book_delete_post,
-  book_update_get,
-  book_update_post
+  index,
+  show,
+  create_form,
+  create,
+  destroy_form,
+  destroy,
+  update_form,
+  update
 }
