eb83715e50ff663d19899017976140a8c5eb07a9
Vladimir Suvorov <bluurn@gmail.com>
2018-12-01 21:54:36 +0300

    rename author action names

--- controllers/author/controller.js
+++ controllers/author/controller.js
@@ -1,19 +1,19 @@
-import author_list from './actions/index';
-import author_detail from './actions/show';
-import author_create_get from './actions/create_form';
-import author_create_post from './actions/create';
-import author_delete_get from './actions/delete_form';
-import author_delete_post from './actions/delete';
-import author_update_get from './actions/update_form';
-import author_update_post from './actions/update';
+import index from './actions/index';
+import show from './actions/show';
+import create_form from './actions/create_form';
+import create from './actions/create';
+import destroy_form from './actions/destroy_form';
+import destroy from './actions/destroy';
+import update_form from './actions/update_form';
+import update from './actions/update';
 
 export default {
-    author_list,
-    author_detail,
-    author_create_get,
-    author_create_post,
-    author_delete_get,
-    author_delete_post,
-    author_update_get,
-    author_update_post
+    index,
+    show,
+    create_form,
+    create,
+    destroy_form,
+    destroy,
+    update_form,
+    update
 }
